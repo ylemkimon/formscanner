db6b0204c7b5e3c10e29743c634bde6a1cf9ad78
superalberto76 <superalberto76@gmail.com>
2013-05-31 17:20:42 +0200

    #10 Add statusbar to ImageFrame

--- formscanner-main/src/main/java/org/albertoborsetta/formscanner/gui/ImageFrame.java
+++ formscanner-main/src/main/java/org/albertoborsetta/formscanner/gui/ImageFrame.java
@@ -12,12 +12,15 @@ import org.albertoborsetta.formscanner.gui.controller.ImageFrameController;
 import org.albertoborsetta.formscanner.model.FormScannerModel;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.BorderLayout;
+import java.awt.SystemColor;
 import java.awt.image.BufferedImage;
 
+import javax.swing.Icon;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -83,6 +86,43 @@ public class ImageFrame extends JInternalFrame implements ScrollableImageView {
 	
 	private class ImageStatusBar extends JPanel {
 		
+		private class AngledLinesWindowsCornerIcon implements Icon {
+			  private Color WHITE_LINE_COLOR = new Color(255, 255, 255);
+
+			  private Color GRAY_LINE_COLOR = new Color(172, 168, 153);
+			  private static final int WIDTH = 13;
+
+			  private static final int HEIGHT = 13;
+
+			  public int getIconHeight() {
+			    return WIDTH;
+			  }
+
+			  public int getIconWidth() {
+			    return HEIGHT;
+			  }
+
+			  public void paintIcon(Component c, Graphics g, int x, int y) {
+
+			    g.setColor(WHITE_LINE_COLOR);
+			    g.drawLine(0, 12, 12, 0);
+			    g.drawLine(5, 12, 12, 5);
+			    g.drawLine(10, 12, 12, 10);
+
+			    g.setColor(GRAY_LINE_COLOR);
+			    g.drawLine(1, 12, 12, 1);
+			    g.drawLine(2, 12, 12, 2);
+			    g.drawLine(3, 12, 12, 3);
+
+			    g.drawLine(6, 12, 12, 6);
+			    g.drawLine(7, 12, 12, 7);
+			    g.drawLine(8, 12, 12, 8);
+
+			    g.drawLine(11, 12, 12, 11);
+			    g.drawLine(12, 12, 12, 12);
+
+			  }
+			}
 		/**
 		 * 
 		 */
@@ -91,9 +131,16 @@ public class ImageFrame extends JInternalFrame implements ScrollableImageView {
 		public ImageStatusBar() {
 			super();
 			setFont(FormScannerFont.getFont());
-			JLabel label = new JLabel("pippo");
 			setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-			add(label);
+			setLayout(new BorderLayout());
+		    setPreferredSize(new Dimension(10, 23));
+
+		    JPanel rightPanel = new JPanel(new BorderLayout());
+		    rightPanel.add(new JLabel(new AngledLinesWindowsCornerIcon()), BorderLayout.SOUTH);
+		    rightPanel.setOpaque(false);
+
+		    add(rightPanel, BorderLayout.EAST);
+		    // setBackground(SystemColor.control);
 		}
 	}
 	
