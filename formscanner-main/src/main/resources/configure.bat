be7c755e158024209f27a90856c89ec808a962a6
Alberto Borsetta <a.borsetta@tecnoteca.com>
2013-07-02 14:52:28 +0200

    #8 General refactoring

--- formscanner-main/src/main/resources/configure.bat
+++ formscanner-main/src/main/resources/configure.bat
@@ -14,120 +14,14 @@ rem #    You should have received a copy of the GNU General Public License
 rem #    along with this program. If not, see http://www.gnu.org/licenses
 rem #-----------------------------------------------------------------------
 @echo off
-cls
-rem *********************************************
-rem *  Initialize environment variables
-rem *********************************************
+for /f %%j in ("java.exe") do (
+    set JAVA_PATH=%%~dp$PATH:j
+)
 
-SET SET_JDKHOME=off
+set INSTALL_DIR=%CD%
 
-SET JDKHOME=%JAVA_HOME%
+for /f "delims=" %%f in ('dir /s /b /a-d "%INSTALL_DIR%"\lib\formscanner*') do (
+	set FS_EXECUTABLE=%%~f
+)
 
-if exist configure.properties goto init
-
-rem *********************************************
-rem *  Set properties values from user input
-rem *********************************************
-:start
-if %~1.==. goto make
-if %~1==-help goto help
-if %~1==-jdkhome goto jdkhome
-goto error
-
-rem *********************************************
-rem *  Set default values
-rem *********************************************
-:default
-SET JDKHOME=%JAVA_HOME%
-
-goto make
-
-
-rem *********************************************
-rem *  Init configure.properties parameters
-rem *********************************************
-:init
-find "jdk.dir" < configure.properties > javadir.txt
-for /F "tokens=1,2* delims==" %%i in (javadir.txt) do SET JDKHOME=%%j
-del javadir.txt>nul
-if "X%JDKHOME%"=="X" goto initjava
-goto start
-
-:initjava
-JDKHOME=%JAVA_HOME%
-goto start
-
-rem *********************************************************
-rem *  Edit parameters (configure.properties)
-rem *********************************************************
-:make
-if exist configure.properties del configure.properties
-echo jdk.dir=^%JDKHOME%>configure.properties
-
-set OLDCLASSPATH=%CLASSPATH%
-set OLDPATH=%PATH%
-set CLASSPATH="%CD%\lib\ant.jar"
-set CLASSPATH=%CLASSPATH%;"%CD%\lib\ant-launcher.jar"
-set CLASSPATH=%CLASSPATH%;"%CD%\lib\xercesImpl.jar"
-
-set PATH="%JDKHOME%\bin"
-java -cp %CLASSPATH% org.apache.tools.ant.Main -DSYSTEMROOT=%SYSTEMROOT%
-
-set PATH=%OLDPATH%
-set CLASSPATH=%OLDCLASSPATH%
-set OLDCLASSPATH=
-set OLDPATH=
-
-goto end
-
-
-rem *********************************************************
-rem *  Display ERROR message
-rem *********************************************************
-:error
-echo.
-echo Invalid options using with configure.bat
-echo.
-
-
-rem *********************************************************
-rem *  Display HELP message
-rem *********************************************************
-:help
-echo.
-echo Parameters value for using with configure.bat :
-echo.
-echo configure       - Make configure.properties file with default 'jdk.dir' value
-echo.
-echo configure -help - Display this screen
-echo.
-echo configure [-jdkhome jdk_home_dir]
-echo.
-echo.
-echo Examples :
-echo.
-echo configure -jdkhome C:/jdk1.6.0_20
-echo.
-goto end
-
-rem *********************************************************
-rem *  Set JDKHOME parameter value
-rem *********************************************************
-:jdkhome
-if %SET_JDKHOME%==on goto error
-shift
-if "X%~1"=="X" goto error
-SET JDKHOME=%~f1
-SET SET_JDKHOME=on
-shift
-if "X%~1"=="X" goto make
-goto error
-
-
-rem *********************************************************
-rem *  Reset evironment variables
-rem *********************************************************
-:end
-SET JDKHOME=
-
-SET SET_JDKHOME=
+echo "%JAVA_PATH%"java -jar -DFormScanner_HOME="%INSTALL_DIR%" "%FS_EXECUTABLE%" > %INSTALL_DIR%"\bin\run.bat"
