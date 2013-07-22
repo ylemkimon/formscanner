a65967f9fd16ee84d7bbf2b23fbd320538cb7769
superalberto76 <superalberto76@gmail.com>
2013-07-22 22:59:02 +0200

    Starting 0.5 branch

---
+++ formscanner-main/src/main/java/org/albertoborsetta/formscanner/gui/controller/ImageFrameController.java
@@ -0,0 +1,140 @@
+package org.albertoborsetta.formscanner.gui.controller;
+
+import java.awt.Cursor;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.event.MouseInputListener;
+
+import org.albertoborsetta.formscanner.commons.FormPoint;
+import org.albertoborsetta.formscanner.gui.ImageFrame;
+import org.albertoborsetta.formscanner.model.FormScannerModel;
+
+public class ImageFrameController implements MouseMotionListener, MouseInputListener {
+	
+	private FormScannerModel model;
+	private ImageFrame view;
+	private FormPoint p1;
+	private FormPoint p2;
+	
+	public ImageFrameController(FormScannerModel model) {
+		this.model = model;
+	}
+	
+	public void add(ImageFrame view) {
+		this.view = view;
+	}
+
+	// MouseMotionListener
+	public void mouseDragged(MouseEvent e) {
+		switch (view.getMode()) {
+		case UPDATE:
+			if (e.isControlDown()) {
+				p2 = new FormPoint(e.getPoint());
+				
+				double deltaX = p1.getX() - p2.getX();
+				double deltaY = p1.getY() - p2.getY();
+				p1 = p2;
+				
+				model.setScrollBars(view, (int) deltaX, (int) deltaY);
+				model.setImageCursor(view, new Cursor(Cursor.HAND_CURSOR));
+			}
+			break;
+		case VIEW:
+			p2 =  new FormPoint(e.getPoint());
+			
+			double deltaX = p1.getX() - p2.getX();
+			double deltaY = p1.getY() - p2.getY();
+			p1 = p2;
+			
+			model.setScrollBars(view, (int) deltaX, (int) deltaY);
+			break;
+		default:
+			break;
+		}
+				
+	}
+
+	public void mouseMoved(MouseEvent e) {
+	}
+
+	// MouseImputListener
+	public void mouseClicked(MouseEvent e) {
+		switch (view.getMode()) {
+		case UPDATE:
+			if (!e.isControlDown()) {
+				switch (e.getButton()) {
+				case MouseEvent.BUTTON1:
+					FormPoint p = new FormPoint(e.getPoint());
+					
+					int	dx = view.getHorizontalScrollbarValue();
+					int dy = view.getVerticalScrollbarValue();
+					
+					double x = p.getX()+dx;
+					double y = p.getY()+dy;
+					
+					FormPoint p1 = new FormPoint(x, y);
+					model.addPoint(view, p1);
+					break;
+				case MouseEvent.BUTTON3:
+					break;
+				}			
+			}
+			break;
+		case VIEW:
+		default:
+			break;
+		}
+	}
+
+	public void mousePressed(MouseEvent e) {
+		switch (view.getMode()) {
+		case UPDATE:
+			if (e.isControlDown()) {
+				p1 = new FormPoint(e.getPoint());
+				model.setImageCursor(view, new Cursor(Cursor.HAND_CURSOR));
+			}
+			break;
+		case VIEW:
+			p1 = new FormPoint(e.getPoint());
+			break;
+		default:
+			break;
+		}
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		switch (view.getMode()) {
+		case UPDATE:
+			if (e.isControlDown()) {
+				model.setImageCursor(view, new Cursor(Cursor.CROSSHAIR_CURSOR));
+			}
+			break;
+		case VIEW:
+		default:
+			break;
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		switch (view.getMode()) {
+		case UPDATE:
+			model.setImageCursor(view, new Cursor(Cursor.CROSSHAIR_CURSOR));
+			break;
+		case VIEW:
+			model.setImageCursor(view, new Cursor(Cursor.HAND_CURSOR));
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void mouseExited(MouseEvent e) {
+		model.setImageCursor(view, new Cursor(Cursor.DEFAULT_CURSOR));
+	}
+
+//	public void mouseWheelMoved(MouseWheelEvent e) {
+//		model.zoomImage(view, e.getWheelRotation());
+//	}
+}
