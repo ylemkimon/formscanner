4d279db505519846339ff0a4a16e7bacf6bccfd3
Superalberto76 <Superalberto76@localhost>
2014-03-13 13:07:47 +0100

    #21: Frame flickering when moved

--- formscanner-main/src/main/java/org/albertoborsetta/formscanner/gui/controller/ImageFrameController.java
+++ formscanner-main/src/main/java/org/albertoborsetta/formscanner/gui/controller/ImageFrameController.java
@@ -2,15 +2,17 @@ package org.albertoborsetta.formscanner.gui.controller;
 
 import java.awt.Cursor;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
 
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelListener;
 import javax.swing.event.MouseInputListener;
 
 import org.albertoborsetta.formscanner.commons.FormPoint;
 import org.albertoborsetta.formscanner.gui.ImageFrame;
 import org.albertoborsetta.formscanner.model.FormScannerModel;
 
-public class ImageFrameController implements MouseMotionListener, MouseInputListener {
+public class ImageFrameController implements MouseMotionListener, MouseInputListener, MouseWheelListener {
 	
 	private FormScannerModel model;
 	private ImageFrame view;
@@ -29,25 +31,21 @@ public class ImageFrameController implements MouseMotionListener, MouseInputList
 	public void mouseDragged(MouseEvent e) {
 		switch (view.getMode()) {
 		case UPDATE:
-			if (e.isControlDown()) {
-				p2 = new FormPoint(e.getPoint());
-				
-				double deltaX = p1.getX() - p2.getX();
-				double deltaY = p1.getY() - p2.getY();
-				p1 = p2;
-				
-				model.setScrollBars(view, (int) deltaX, (int) deltaY);
-				model.setImageCursor(view, new Cursor(Cursor.HAND_CURSOR));
+			if (!e.isControlDown()) {
+				break;
 			}
-			break;
+			// model.setImageCursor(view, new Cursor(Cursor.HAND_CURSOR));
 		case VIEW:
 			p2 =  new FormPoint(e.getPoint());
 			
-			double deltaX = p1.getX() - p2.getX();
-			double deltaY = p1.getY() - p2.getY();
-			p1 = p2;
-			
-			model.setScrollBars(view, (int) deltaX, (int) deltaY);
+			if (p1.dist2(p2) >= 10) {
+				double deltaX = (p1.getX() - p2.getX())*2;
+				double deltaY = (p1.getY() - p2.getY())*2;
+				
+				p1 = p2;
+				
+				model.setScrollBars(view, (int) deltaX, (int) deltaY);
+			}			
 			break;
 		default:
 			break;
@@ -56,9 +54,10 @@ public class ImageFrameController implements MouseMotionListener, MouseInputList
 	}
 
 	public void mouseMoved(MouseEvent e) {
+		System.out.println(e.getX() + ", " + e.getY());
 	}
 
-	// MouseImputListener
+	// MouseInputListener
 	public void mouseClicked(MouseEvent e) {
 		switch (view.getMode()) {
 		case UPDATE:
@@ -134,7 +133,13 @@ public class ImageFrameController implements MouseMotionListener, MouseInputList
 		model.setImageCursor(view, new Cursor(Cursor.DEFAULT_CURSOR));
 	}
 
-//	public void mouseWheelMoved(MouseWheelEvent e) {
-//		model.zoomImage(view, e.getWheelRotation());
-//	}
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		int delta = e.getUnitsToScroll()*5;
+		
+		if (e.isControlDown()) {
+			model.setScrollBars(view, delta, 0);
+		} else {
+			model.setScrollBars(view, 0, delta);
+		}
+	}
 }
