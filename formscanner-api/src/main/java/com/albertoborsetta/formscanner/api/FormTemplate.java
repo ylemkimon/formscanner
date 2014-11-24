42a35b11986de5eea6deb2a44cb80c3e48638a31
Alberto Borsetta <superalberto76@gmail.com>
2014-11-24 12:11:04 +0100

    58: Expose core functionalities through APIs
    Task-Url: http://www.bugheaven.com/bugzilla/FormScanner/show_bug.cgi?id=58

--- formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/FormTemplate.java
+++ formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/FormTemplate.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
