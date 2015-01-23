06462c4cc22febb20470f722d83b3fb6f7a2f9d3
Alberto Borsetta <superalberto76@gmail.com>
2015-01-23 18:08:53 +0100

    68: Support for RTL languages
    Task-Url: http://www.bugheaven.com/bugzilla/FormScanner/show_bug.cgi?id=68

--- formscanner-main/src/main/java/com/albertoborsetta/formscanner/gui/builder/PanelBuilder.java
+++ formscanner-main/src/main/java/com/albertoborsetta/formscanner/gui/builder/PanelBuilder.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.util.ArrayList;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
@@ -16,69 +16,143 @@ import org.uncommons.swing.SpringUtilities;
 import com.albertoborsetta.formscanner.commons.FormScannerFont;
 
 public class PanelBuilder {
+
+	private ComponentOrientation orientation;
+	private Color bgColor = null;
+	private Object layout = null;
+	private Border border = null;
+	private ArrayList<JComponent> components = new ArrayList<JComponent>();
+	private ArrayList<String> positions = new ArrayList<String>();
+	private Dimension size = null;
+	private int rows = 0;
+	private int cols = 0;
 	
-	protected JPanel panel;
+	public PanelBuilder() {
+		this.orientation = ComponentOrientation.LEFT_TO_RIGHT;
+	}
 	
 	public PanelBuilder(ComponentOrientation orientation) {
-		panel = new JPanel();
-		panel.setFont(FormScannerFont.getFont());
-		panel.setComponentOrientation(orientation);
+		this.orientation = orientation;
 	}
-	
+
 	public PanelBuilder withBackgroundColor(Color color) {
-		panel.setBackground(color);
+		this.bgColor = color;
 		return this;
 	}
-	
-	public PanelBuilder withLayout(BorderLayout layout) {
-		panel.setLayout(layout);
+
+	public PanelBuilder withLayout(Object layout) {
+		this.layout = layout;
 		return this;
 	}
-	
-	public PanelBuilder withLayout(SpringLayout layout) {
-		panel.setLayout(layout);
-		return this;
-	}
-	
+
 	public PanelBuilder withGrid(int rows, int cols) {
-		SpringUtilities.makeCompactGrid(panel, rows, cols, 3, 3, 3, 3);
+		this.rows = rows;
+		this.cols = cols;
 		return this;
 	}
-	
+
 	public PanelBuilder withPreferredSize(Dimension size) {
-		panel.setPreferredSize(size);
+		this.size = size;
 		return this;
 	}
-	
+
 	public PanelBuilder addComponent(JComponent component, String position) {
-		panel.add(component, position);
+		components.add(component);
+		positions.add(position);
 		return this;
 	}
-	
+
 	public PanelBuilder add(JComponent component, String position) {
 		return addComponent(component, position);
 	}
-	
-	public JPanel build() {
-		return panel;
-	}
-	
-	public PanelBuilder withLayout(FlowLayout layout) {
-		panel.setLayout(layout);
-		return this;
-	}
-	
+
 	public PanelBuilder withBorder(Border border) {
-		panel.setBorder(border);
+		this.border = border;
 		return this;
 	}
-	
+
 	public PanelBuilder addComponent(JComponent component) {
-		panel.add(component);
+		components.add(component);
 		return this;
 	}
-	
+
 	public PanelBuilder add(JComponent component) {
 		return addComponent(component);
+	}
+
+	public JPanel build() {
+		JPanel panel = new JPanel();
+		panel.setFont(FormScannerFont.getFont());
+		panel.setComponentOrientation(orientation);
+
+		if (bgColor != null) {
+			panel.setBackground(bgColor);
+		}
+
+		if (border != null) {
+			panel.setBorder(border);
+		}
+
+		switch (layout.getClass().getName()) {
+		case "java.awt.BorderLayout":
+			panel.setLayout((BorderLayout) layout);
+			
+			if (!orientation.isLeftToRight()) {
+				switchEastWestPositions();
+			}
+			
+			for (int i = 0; i < components.size(); i++) {
+				panel.add(components.get(i), positions.get(i));
+			}
+			break;
+		case "javax.swing.SpringLayout":
+			panel.setLayout((SpringLayout) layout);
+
+			if (!orientation.isLeftToRight()) {
+				switchGridPositions();
+			}
+			
+			for (int i = 0; i < components.size(); i++) {	 
+				panel.add(components.get(i));
+			}
+			SpringUtilities.makeCompactGrid(panel, rows, cols, 3, 3, 3, 3);
+			break;
+		}
+
+		if (size != null) {
+			panel.setPreferredSize(size);
+		}
+
+		return panel;
+	}
+
+	private void switchGridPositions() {
+		int size = components.size();
+		if ((size % 2) == 0) {
+			for (int i=0; i<size; i=i+2) {
+				JComponent tempComponent = components.get(i);
+				components.set(i, components.get(i+1));
+				components.set(i+1, tempComponent);
+			}
+		} else {
+			for (int i=0; i<size/2; i++) {
+				JComponent tempComponent = components.get(i);
+				components.set(i, components.get(size-i));
+				components.set(size-i, tempComponent);
+			}
+		}
+	}
+
+	private void switchEastWestPositions() {
+		for (int i=0; i<positions.size(); i++) {
+			switch (positions.get(i)) {
+			case BorderLayout.EAST:
+				positions.set(i, BorderLayout.WEST);
+				break;
+			case BorderLayout.WEST:
+				positions.set(i, BorderLayout.EAST);
+				break;
+			}
+		}
 	}
 }
