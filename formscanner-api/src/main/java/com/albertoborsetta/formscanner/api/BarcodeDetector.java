843a7bad7d5603bdabdca4810a1cb9c4af3af2c4
superalberto76 <superalberto76@localhost>
2015-04-01 14:32:25 +0200

    74: Add support for QR-Code and Barcode scanning
    Task-Url: http://www.bugheaven.com/bugzilla/FormScanner/show_bug.cgi?id=74

--- formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/BarcodeDetector.java
+++ formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/BarcodeDetector.java
@@ -1,14 +1,13 @@
 package com.albertoborsetta.formscanner.api;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.EnumMap;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Callable;
 
+import com.albertoborsetta.formscanner.api.commons.Constants;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
@@ -22,20 +21,21 @@ import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.multi.GenericMultipleBarcodeReader;
 import com.google.zxing.multi.MultipleBarcodeReader;
 import com.google.zxing.Result;
-import com.google.common.collect.Lists;
 
 public class BarcodeDetector implements Callable<HashMap<String, FormArea>> {
 	
-	private static final Map<DecodeHintType,Object> HINTS;
-	private static final Map<DecodeHintType,Object> HINTS_PURE;
+//	private static final Map<DecodeHintType,Object> HINTS;
+//	private static final Map<DecodeHintType,Object> HINTS_PURE;
+//	
+//	static {
+//	    HINTS = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
+//	    HINTS.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+//	    HINTS.put(DecodeHintType.POSSIBLE_FORMATS, EnumSet.allOf(BarcodeFormat.class));
+//	    HINTS_PURE = new EnumMap<DecodeHintType, Object>(HINTS);
+//	    HINTS_PURE.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+//	  }
 	
-	static {
-	    HINTS = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
-	    HINTS.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
-	    HINTS.put(DecodeHintType.POSSIBLE_FORMATS, EnumSet.allOf(BarcodeFormat.class));
-	    HINTS_PURE = new EnumMap<DecodeHintType, Object>(HINTS);
-	    HINTS_PURE.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
-	  }
+	
 	
 	private FormTemplate template;
 	private FormArea barcodeArea;
@@ -53,7 +53,7 @@ public class BarcodeDetector implements Callable<HashMap<String, FormArea>> {
 	public HashMap<String, FormArea> call() throws Exception {
 		LuminanceSource source = new BufferedImageLuminanceSource(image);
 	    BinaryBitmap bitmap = new BinaryBitmap(new GlobalHistogramBinarizer(source));
-	    Collection<Result> results = Lists.newArrayListWithCapacity(1);
+	    ArrayList<Result> results = new ArrayList<Result>();
 
 	    try {
 
@@ -62,7 +62,7 @@ public class BarcodeDetector implements Callable<HashMap<String, FormArea>> {
 	      try {
 	        // Look for multiple barcodes
 	        MultipleBarcodeReader multiReader = new GenericMultipleBarcodeReader(reader);
-	        Result[] theResults = multiReader.decodeMultiple(bitmap, HINTS);
+	        Result[] theResults = multiReader.decodeMultiple(bitmap, Constants.HINTS);
 	        if (theResults != null) {
 	          results.addAll(Arrays.asList(theResults));
 	        }
@@ -73,7 +73,7 @@ public class BarcodeDetector implements Callable<HashMap<String, FormArea>> {
 	      if (results.isEmpty()) {
 	        try {
 	          // Look for pure barcode
-	          Result theResult = reader.decode(bitmap, HINTS_PURE);
+	          Result theResult = reader.decode(bitmap, Constants.HINTS_PURE);
 	          if (theResult != null) {
 	            results.add(theResult);
 	          }
@@ -85,7 +85,7 @@ public class BarcodeDetector implements Callable<HashMap<String, FormArea>> {
 	      if (results.isEmpty()) {
 	        try {
 	          // Look for normal barcode in photo
-	          Result theResult = reader.decode(bitmap, HINTS);
+	          Result theResult = reader.decode(bitmap, Constants.HINTS);
 	          if (theResult != null) {
 	            results.add(theResult);
 	          }
@@ -98,7 +98,7 @@ public class BarcodeDetector implements Callable<HashMap<String, FormArea>> {
 	        try {
 	          // Try again with other binarizer
 	          BinaryBitmap hybridBitmap = new BinaryBitmap(new HybridBinarizer(source));
-	          Result theResult = reader.decode(hybridBitmap, HINTS);
+	          Result theResult = reader.decode(hybridBitmap, Constants.HINTS);
 	          if (theResult != null) {
 	            results.add(theResult);
 	          }
