fba904bcaee27274fe5fd7f3bd06560ea5ddbcb8
Alberto Borsetta <superalberto76@gmail.com>
2015-08-16 08:43:49 +0200

    30: General refactoring
    Task-Url: http://www.bugheaven.com/bugzilla/FormScanner/show_bug.cgi?id=30

--- formscanner-gui/src/main/java/com/albertoborsetta/formscanner/gui/AboutFrame.java
+++ formscanner-gui/src/main/java/com/albertoborsetta/formscanner/gui/AboutFrame.java
@@ -35,8 +35,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import  javafx.application.Application;
-
 public class AboutFrame extends InternalFrame {
 
 	/**
