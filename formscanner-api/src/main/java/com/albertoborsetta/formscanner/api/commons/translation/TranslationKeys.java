324e09b9f25f647983b6399001bea6abe74ef99c
Alberto Borsetta <superalberto76@gmail.com>
2015-08-20 16:40:04 +0200

    Switch to 1.1 in the default branch

--- formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/commons/translation/TranslationKeys.java
+++ formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/commons/translation/TranslationKeys.java
@@ -4,7 +4,7 @@ package com.albertoborsetta.formscanner.api.commons.translation;
  * The Translation Keys Constants.
  *
  * @author Alberto Borsetta
- * @version 0.12-SNAPSHOT
+ * @version 1.1-SNAPSHOT
  */
 public class TranslationKeys {
 
