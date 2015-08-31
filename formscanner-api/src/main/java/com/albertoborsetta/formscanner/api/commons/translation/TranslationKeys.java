394f68f9b6811d5320d21a1851748ba12e8e70c6
superalberto76 <superalberto76@localhost>
2015-08-31 15:58:10 +0200

    [Build] Updated release maintenance branch version

--- formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/commons/translation/TranslationKeys.java
+++ formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/commons/translation/TranslationKeys.java
@@ -4,7 +4,7 @@ package com.albertoborsetta.formscanner.api.commons.translation;
  * The Translation Keys Constants.
  *
  * @author Alberto Borsetta
- * @version 1.0.1
+ * @version 1.0.2-SNAPSHOT
  */
 public class TranslationKeys {
 
