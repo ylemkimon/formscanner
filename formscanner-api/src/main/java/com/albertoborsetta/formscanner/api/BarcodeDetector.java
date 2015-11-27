f3236ce30f25e5076f9556dc46c92a6bdd97d69b
Alberto Borsetta <superalberto76@gmail.com>
2015-11-27 16:55:33 +0100

    Release 1.1.1

--- formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/BarcodeDetector.java
+++ formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/BarcodeDetector.java
@@ -22,7 +22,7 @@ import com.google.zxing.Result;
 /**
 *
 * @author Alberto Borsetta
-* @version 1.1.1-SNAPSHOT
+* @version 1.1.1
 */
 public class BarcodeDetector extends FormScannerDetector
 		implements Callable<HashMap<String, FormArea>> {
--- formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/CornerDetector.java
+++ formscanner-api/src/main/java/com/albertoborsetta/formscanner/api/CornerDetector.java
@@ -10,7 +10,7 @@ import com.albertoborsetta.formscanner.api.commons.Constants.Corners;
 /**
 *
 * @author Alberto Borsetta
-* @version 1.1.1-SNAPSHOT
+* @version 1.1.1
 */
 public class CornerDetector extends FormScannerDetector
 		implements Callable<FormPoint> {
